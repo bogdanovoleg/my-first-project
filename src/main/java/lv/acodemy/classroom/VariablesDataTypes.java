d0dcb406d899b7ed44ebf368e07af946c474c6bd
bogdanovoleg <novijdenj@gmail.com>
2023-03-01 20:50:12 +0200

    [classroom_01032023]: Add classroom progress/

--- src/main/java/lv/acodemy/classroom/VariablesDataTypes.java
+++ src/main/java/lv/acodemy/classroom/VariablesDataTypes.java
@@ -75,6 +75,7 @@ public class VariablesDataTypes {
 
         int[] numbers = {0, 1, 2 ,3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Arrays.toString(numbers));
-        for int
+
+        
     }
 }
