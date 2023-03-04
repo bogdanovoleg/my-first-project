ccbce6998a1261971f0653a9197f231d238bae74
bogdanovoleg <novijdenj@gmail.com>
2023-03-05 00:13:51 +0200

    [Homework_01032023]: Add Homework first exercise

---
+++ src/main/java/lv/acodemy/Homework/VariablesDataTypes1.java
@@ -0,0 +1,30 @@
+package lv.acodemy.Homework;
+
+import java.util.Arrays;
+
+public class VariablesDataTypes1 {
+    public static void main(String[] args) {
+    byte myCurrentAge = 29;
+    byte monthsInYear = 12;
+    byte carsLoanYears = 5;
+    byte flatsMortgageYears = 20;
+    short monthlySalary = 2000;
+    short carsPrice = 1000;
+    int flatsPrice = 70000;
+    int dateOfBirth = 28071993;
+    long populationOfEurope = 746400000;
+    long kilometersDrivenByCar = 210000;
+    char examMark1 = 'B';
+    char examMark2 = 'C';
+    float iosVersion = 16.3f;
+    float endCurseDate = 11.04f;
+    double currencyEuroPound = 0.88;
+    double MortgageAnnualPercentage = 1.9;
+    boolean mortgageApproved = true;
+    boolean lastIosVersion = true;
+    if (iosVersion == 16.3f) {
+        System.out.println(lastIosVersion);
+    }
+
+    }
+}
