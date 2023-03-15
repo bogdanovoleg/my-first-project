69f08809a19f96a8c0ac9b30dea27d8577efb09b
bogdanovoleg <novijdenj@gmail.com>
2023-03-15 19:57:33 +0200

    [classroom_15032023]: Test

---
+++ src/main/java/lv/acodemy/classroom/Calculator.java
@@ -0,0 +1,19 @@
+package lv.acodemy.classroom;
+
+public class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return (firstNumber + secondNumber);
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return (firstNumber - secondNumber);
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return (firstNumber * secondNumber);
+    }
+
+    public int divide(int firstNumber, int secondNumber) {
+        return (firstNumber / secondNumber);
+    }
+}
